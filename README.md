e3937b76f964af6710a88321773124a72280e31e
github-usman <usmantoyou@gmail.com>
2023-11-13 06:16:37 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# deploy-proxy-server-nov-thirteen
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/',(req,res)=>res.json(
+    {
+    message:"Hello Proxy server",
+}))
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+  });
